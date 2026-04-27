c9a37d332ebcf6cb349be90cacde0f19339eb2e1
Abhishek <abhishekgera2001@gmail.com>
2026-04-28 02:12:45 +0530

    made changes in index.js and app.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import ItemDetail from "./pages/ItemDetail.jsx";
 import NotFound from "./pages/NotFound.jsx";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
